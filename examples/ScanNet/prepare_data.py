e2e164ceb0ebde21a18ec3a18f05327560f07a95
xuyongzhi <buaaxyz@yeah.net>
2018-12-03 11:54:28 +1100

    slightly modify examples/ScanNet/prepare_data.py fix label bug, change save path

--- examples/ScanNet/prepare_data.py
+++ examples/ScanNet/prepare_data.py
@@ -4,27 +4,37 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import glob, plyfile, numpy as np, multiprocessing as mp, torch
+import glob, plyfile, numpy as np, multiprocessing as mp, torch, os
 
 # Map relevant classes to {0,1,...,19}, and ignored classes to -100
 remapper=np.ones(150)*(-100)
 for i,x in enumerate([1,2,3,4,5,6,7,8,9,10,11,12,14,16,24,28,33,34,36,39]):
     remapper[x]=i
 
-files=sorted(glob.glob('*/*_vh_clean_2.ply'))
-files2=sorted(glob.glob('*/*_vh_clean_2.labels.ply'))
+root_path = '/DT/SCANNET'
+save_path = '/DS/ScanNet/SparseCNN'
+files=sorted(glob.glob('%s/*/*_vh_clean_2.ply'%(root_path)))
+files2=sorted(glob.glob('%s/*/*_vh_clean_2.labels.ply'%(root_path)))
 assert len(files) == len(files2)
 
 def f(fn):
     fn2 = fn[:-3]+'labels.ply'
     a=plyfile.PlyData().read(fn)
-    v=np.array([list(x) for x in a.elements[0]])
-    coords=np.ascontiguousarray(v[:,:3]-v[:,:3].mean(0))
-    colors=np.ascontiguousarray(v[:,3:6])/127.5-1
+    v=np.array([list(x) for x in a.elements[0]]) # (81369, 7)
+    coords=np.ascontiguousarray(v[:,:3]-v[:,:3].mean(0)) # (81369, 3)
+    colors=np.ascontiguousarray(v[:,3:6])/127.5-1 #  colors
     a=plyfile.PlyData().read(fn2)
-    w=remapper[np.array(a.elements[0]['label'])]
-    torch.save((coords,colors,w),fn[:-4]+'.pth')
-    print(fn, fn2)
+    label = np.array(a.elements[0]['label'])
+    label = np.minimum(label, 149)
+    w=remapper[label]
+    fn_3 = (fn[:-4]+'.pth').replace(root_path, save_path)
+    dir_3 = os.path.dirname(fn_3)
+    if not os.path.exists(dir_3):
+      os.makedirs(dir_3)
+    torch.save((coords,colors,w),fn_3)
+    print(fn, fn2, fn_3)
+
+#f(files[0])
 
 p = mp.Pool(processes=mp.cpu_count())
 p.map(f,files)
